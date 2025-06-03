a1dcec5734330fcb40eb1d072b5e8af72ca2e0c6
zylm00 <1123317122@qq.com>
2025-06-04 01:48:10 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://192.238.141.23:21114".to_owned()
 }
 
 #[inline]
